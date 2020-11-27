3051cc80b4ab529818bd40f5edcb10403da162c0
Ehsan <ehcan@icloud.com>
2020-11-27 19:00:31 +0330

    add jsdoc + update axios

--- app.js
+++ app.js
@@ -1,36 +1,61 @@
 const axios = require('axios').default;
 
 class Myket {
-    constructor(access_token, package_name) {
-        this.access_token = access_token;
-        this.package_name = package_name;
-    }
+	/**
+	 * Get an instance
+	 * @constructs
+	 * @param {string} access_token
+	 * @param {string} package_name
+	 * @returns {Myket}
+	 */
+	constructor(access_token, package_name) {
+		/**
+		 * Your access_token From Myket
+		 * @private
+		 */
+		this.access_token = access_token;
+		/**
+		 * Your package_name From Myket
+		 * @private
+		 */
+		this.package_name = package_name;
+	}
 
-    validate = (sku, token) => new Promise(async (resolve, reject) => {
-        try {
-            axios({
-                method: 'get',
-                headers: { 'X-Access-Token': this.access_token },
-                url: `https://developer.myket.ir/api/applications/${this.package_name}/purchases/products/${sku}/tokens/${token}`
-            })
-                .then(response => {
-                    let ret = {
-                        status: response.status,
-                        data: response.data
-                    };
-                    return resolve(ret);
-                })
-                .catch(err => {
-                    let ret = {
-                        status: err.response.status,
-                        data: err.response.data
-                    };
-                    return resolve(ret);
-                })
-        } catch (err) {
-            return resolve(err);
-        }
+	/**
+	 * Validate Payment
+	 * @param {string} sku
+	 * @param {string} token
+	 * @returns {Promise<{status: Number, data: {consumptionState: Number, purchaseState: Number, kind: string, developerPayload: string, purchaseTime: Number}}>}
+	 */
+	async validate(sku, token) {
+		try {
+			const response = await axios({
+				method: 'get',
+				headers: { 'X-Access-Token': this.access_token },
+				url: `https://developer.myket.ir/api/applications/${this.package_name}/purchases/products/${sku}/tokens/${token}`,
+			});
 
-    });
+			return {
+				status: response.status,
+				data: response.data,
+			};
+		} catch (err) {
+			return {
+				status: err.response.status,
+				data: err.response.data,
+			};
+		}
+	}
 }
-module.exports = (access_token, package_name) => new Myket(access_token, package_name);
+
+module.exports = getInstance;
+
+/**
+ * Get Instance of Myket
+ * @param {string} access_token
+ * @param {string} package_name
+ * @returns {Myket}
+ */
+function getInstance(access_token, package_name) {
+	return new Myket(access_token, package_name);
+}
